ee0b47dc64d4bef7f20883978c7efb7b3c995868
amanojs <takashivue@gmail.com>
2022-03-22 08:29:55 +0900

    feat(domain/): Tank,Spending,Userエンティティのデータトランスフォームを実装
    基本プロパティの見直し

--- src/architecture/domains/User.ts
+++ src/architecture/domains/User.ts
@@ -1,12 +1,39 @@
 import { DateTimeString } from 'architecture/util/type/DateTime';
 
-export type Userid = string;
-export type Nickname = string;
-
+export type totalBadget = number;
+export type resetDate = number;
 export type User = {
-    userid: Userid;
-    nickname: Nickname;
-    _totalBadget: number | undefined;
-    _updateDay: number | undefined;
-    created: DateTimeString;
+    totalBadget: totalBadget;
+    resetDate: resetDate;
+    // YYYY-MM
+    lastResetMonth: DateTimeString;
 };
+
+export function createUser(payload: { totalBadget: totalBadget; resetDate: resetDate }): User {
+    const user_tmp: User = { ...payload, lastResetMonth: new Date().toISOString() };
+    const user = updateResetLog(user_tmp);
+    return user;
+}
+export function needUpdate(user: User): boolean {
+    const now = new Date();
+    const lastResetMonth = new Date(user.lastResetMonth);
+    if (now.getDate() >= user.resetDate) {
+        return now.getMonth() !== lastResetMonth.getMonth();
+    }
+    return now.getMonth() - 1 !== lastResetMonth.getMonth();
+}
+export function updateResetLog(user: User): User {
+    const now = new Date();
+    if (now.getDate() >= user.resetDate) {
+        const currenMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        return {
+            ...user,
+            lastResetMonth: currenMonth.toISOString()
+        };
+    }
+    const lastMonth = new Date(now.getFullYear(), now.getMonth() - 1, 1);
+    return {
+        ...user,
+        lastResetMonth: lastMonth.toISOString()
+    };
+}
